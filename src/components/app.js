3e3d5ce26798c72e9268dce87e0898214e374ce8
byoung730 <brian.young@monsanto.com>
2018-06-12 10:52:47 -0500

    searchBar

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
 import SearchBar from '../containers/searchBar'
+import _ from 'lodash'
 
 export default class App extends Component {
   render() {
+    const weatherSearch = _.debounce(term => {
+      this.weatherSearch(term)
+    }, 300)
+
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={weatherSearch} />
       </div>
     )
   }
