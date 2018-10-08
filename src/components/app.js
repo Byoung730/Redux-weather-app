54e15cc352e3e47a44ecba98076a93ccaebdf756
byoung730 <brian.young@monsanto.com>
2018-10-08 16:29:13 -0500

    connected weather_list

--- src/components/app.js
+++ src/components/app.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react'
-import SearchBar from '../containers/searchBar'
-import _ from 'lodash'
+import React, { Component } from "react";
+import SearchBar from "../containers/searchBar";
+import WeatherList from "../containers/weather_list";
+import _ from "lodash";
 
 export default class App extends Component {
   render() {
     const weatherSearch = _.debounce(term => {
-      this.weatherSearch(term)
-    }, 300)
+      this.weatherSearch(term);
+    }, 300);
 
     return (
       <div>
         <SearchBar onSearchTermChange={weatherSearch} />
+        <WeatherList />
       </div>
-    )
+    );
   }
 }
