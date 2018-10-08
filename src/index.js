d3d8e9277979ee9b202136810982692fa1acc5ca
byoung730 <brian.young@monsanto.com>
2018-10-08 15:15:07 -0500

    weather reducer added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
-import ReduxPromise from 'redux-promise'
-import App from './components/app'
-import reducers from './reducers'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import ReduxPromise from "redux-promise";
+import App from "./components/app";
+import reducers from "./reducers";
 
-const API_KEY = '4213b8e482cbfd80920f536df6fb3367'
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <App />
   </Provider>,
-  document.querySelector('.container')
-)
+  document.querySelector(".container")
+);
